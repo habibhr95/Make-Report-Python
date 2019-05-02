8cdc04cff59744fe9915b892520b8dbfa2a8d8f0
Md. Habibur Rahman <habib.hr95@gmail.com>
2019-05-02 11:55:18 +0600

    Create create_new_mail.py

---
+++ create_new_mail.py
@@ -0,0 +1,13 @@
+
+def Emailer(text, subject, recipient):
+    import win32com.client as win32
+
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = recipient
+    mail.Subject = subject
+    mail.HtmlBody = text
+    mail.send
+
+Emailer("text","subject h","habib@gmail.com")
+
